1ec2b83a2c1691c4a30203d88408c21b49f3b80f
Aleksi Komulainen <aleksi.komulainen1@gmail.com>
2024-04-04 19:50:54 +0300

    Add get_reviews function

--- reviews.py
+++ reviews.py
@@ -2,6 +2,11 @@ from db import db
 from flask import session
 from sqlalchemy import text
 
+def get_reviews(movie_id):
+    sql = text("SELECT user_id, score, comment FROM reviews WHERE movie_id=:movie_id")
+    reviews = db.session.execute(sql, {"movie_id":movie_id}).fetchall()
+    return reviews
+
 def add_review(movie_id, user_id, score, comment):
     sql = text("""INSERT INTO reviews (movie_id, user_id, score, comment) 
                                 VALUES (:movie_id, :user_id, :score, :comment)""")
