86e4ed5065c84070c7cd640a66a91d89d77df3df
Aleksi Komulainen <aleksi.komulainen1@gmail.com>
2024-05-02 23:02:40 +0300

    Add get_profiles function

--- user_info.py
+++ user_info.py
@@ -7,5 +7,10 @@ def create_profile(user_id, username, fav_movie, fav_genre):
     db.session.execute(sql, {"user_id":user_id, "username":username, "fav_movie":fav_movie, "fav_genre":fav_genre})
     db.session.commit()
 
+def get_profiles(user_id):
+    sql = text("SELECT username, fav_movie, fav_genre FROM user_info WHERE user_id=:user_id")
+    profiles = db.session.execute(sql, {"user_id":user_id}).fetchall()[0]
+    return profiles
+
 def add_fav_movie(fav_movie):
     return
